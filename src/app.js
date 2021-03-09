34ff1bece0942f5685cd7b6bebe7b0ac0e0cf4c1
LauraMargaret Burbach <burbach@uchicago.edu>
2021-03-09 10:20:39 -0600

    test & svg skeleton added

--- src/app.js
+++ src/app.js
@@ -1,27 +1,28 @@
-// if the data you are going to import is small, then you can import it using es6 import
-// (I like to use use screaming snake case for imported json)
-// import MY_DATA from './app/data/example.json'
-
-import {myExampleUtil} from './utils';
-import {select} from 'd3-selection';
-// this command imports the css file, if you remove it your css wont be applied!
+import { csv } from 'd3-fetch';
+import { select } from 'd3-selection';
+import { axisBottom } from 'd3-axis';
 import './main.css';
 
-// this is just one example of how to import data. there are lots of ways to do it!
-fetch('./data/example.json')
-  .then(response => response.json())
-  .then(data => myVis(data))
-  .catch(e => {
-    console.log(e);
-  });
+Promise.all([
+  csv('./data/tiny_summary.csv'),
+  csv('./data/tiny_annual_incidents.csv'),
+  csv('./data/tiny_discipline_by_subgroup.csv')
+]).then(myVis)
+  .catch(error => { console.log(error) });
 
-function myVis(data) {
+function myVis(results) {
+  const [d_summary, d_annual, d_subgroup] = results;
   const width = 5000;
   const height = (36 / 24) * width;
-  console.log(data, height);
-  console.log('Hi!');
-  // EXAMPLE FIRST FUNCTION
-  select('#app')
-    .append('h1')
-    .text('hi!');
+  const margin = { top: 10, left: 50, right: 10, bottom: 50 };
+
+  const svg = select('#app')
+    .append(svg)
+    .attr('height', `${height}px`)
+    .attr('width', `${width}px`)
+    .append('g')
+    .text('DROPDOWN BOXES GO HERE');
+
+  // svg.append('g').attr('class', 'x-axis')
+
 }
