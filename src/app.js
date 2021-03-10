cb159dd162195443668190b6a4df2568c99ca0e9
LauraMargaret Burbach <burbach@uchicago.edu>
2021-03-10 16:34:20 -0600

    adjusted total incidents axis boundaries

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { scaleLinear } from 'd3-scale';
 import { axisBottom, axisLeft, axisRight } from 'd3-axis';
 import { line, polygon, stack, area } from 'd3-shape';
 import { format } from 'd3-format';
+import { extent } from 'd3-array';
 import './main.css';
 
 Promise.all([
@@ -19,58 +20,13 @@ function myVis(results) {
   // const height = (36 / 24) * width;
   // const margin = { top: 20, left: 50, right: 10, bottom: 50 };
 
-  // NUMBER LINE PLOT
-  console.log(d_summary);
-  const margin = { top: 20, right: 30, bottom: 20, left: 60 },
-    width = 400 - margin.left - margin.right,
-    height = 80 - margin.top - margin.bottom;
-
-  const pct1_svg = select("#app")
-    .append('svg')
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", width + margin.top + margin.bottom)
-    .append('g')
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
-
-  pct1_svg.append('text')
-    .attr('class', 'chart-title-small')
-    .text(`Enrollment: ${d_summary[0]['ENROLLMENT']}`);
-
-  const x = scaleLinear().domain([0, 100]).range([0, width])
-  pct1_svg.append('g')
-    .attr("transform", "translate(0," + height + ")")
-    .call(axisBottom(x)
-      .ticks()
-      .tickFormat(format('d'))
-      .tickValues([0, 25, 50, 75, 100]));
-
-  const y = scaleLinear().domain([0, 10]).range([height, 0])
-
-  pct1_svg
-    .append("g")
-    .selectAll("dot")
-    .data(d_summary)
-    .enter()
-    .append("circle")
-    .attr("cx", x(d_summary[0]['ENROLLMENT_PCT']))
-    .attr("cy", y(5))
-    .attr("r", 5)
-    .attr("fill", "steelBlue");
-
-  pct1_svg
-    .append('text')
-    .attr('class', 'footnote')
-    .attr("transform", `translate(0,${height + 2 * (margin.bottom)})`)
-    .text('Percentile compared to all schools; hover for more details');
-
-  // LINE GRAPH
-  // MOSTLY WORKS: QUESTIONS: TEXT LABELS NOT APPEARING
-  // const margin = { top: 20, right: 30, bottom: 10, left: 60 },
+  // // NUMBER LINE PLOT
+  // console.log(d_summary);
+  // const margin = { top: 20, right: 30, bottom: 20, left: 60 },
   //   width = 400 - margin.left - margin.right,
-  //   height = 150 - margin.top - margin.bottom;
+  //   height = 80 - margin.top - margin.bottom;
 
-  // const line_svg = select("#app")
+  // const pct1_svg = select("#app")
   //   .append('svg')
   //   .attr("width", width + margin.left + margin.right)
   //   .attr("height", width + margin.top + margin.bottom)
@@ -78,64 +34,110 @@ function myVis(results) {
   //   .attr("transform",
   //     "translate(" + margin.left + "," + margin.top + ")");
 
-  // line_svg.append('text')
-  //   .attr('class', 'chart-title')
-  //   .text('Total Incidents by School Year');
+  // pct1_svg.append('text')
+  //   .attr('class', 'chart-title-small')
+  //   .text(`Enrollment: ${d_summary[0]['ENROLLMENT']}`);
 
-  // const x_array = [2014, 2015, 2016, 2017]
-  // const y_array = [d_annual[0]['2014_TOTAL_INC'], d_annual[0]['2015_TOTAL_INC'], d_annual[0]['2016_TOTAL_INC'], d_annual[0]['2017_TOTAL_INC']]
-
-  // let data = []
-
-  // for (let i = 0; i < x_array.length; i++) {
-  //   let ob = { x: x_array[i], y: y_array[i] }
-  //   data.push(ob)
-  //   console.log(data)
-  // }
-
-  // const x = scaleLinear().domain([2013.5, 2017.5]).range([0, width])
-  // line_svg.append('g')
+  // const x = scaleLinear().domain([0, 100]).range([0, width])
+  // pct1_svg.append('g')
   //   .attr("transform", "translate(0," + height + ")")
   //   .call(axisBottom(x)
   //     .ticks()
   //     .tickFormat(format('d'))
-  //     .tickValues([2014, 2015, 2016, 2017]));
+  //     .tickValues([0, 25, 50, 75, 100]));
 
-  // const y = scaleLinear().domain([1000, 2500]).range([height, 0])
-  // // line_svg.append('g') //keep commented out
-  // //   .call(axisLeft(y)) //keep commented out
-  // //   .attr('transform', `translate(50,10)`); //keep commented out
+  // const y = scaleLinear().domain([0, 10]).range([height, 0])
 
-  // line_svg.append("path")
-  //   .datum(data)
-  //   .attr("fill", "none")
-  //   .attr("stroke", 'steelBlue')
-  //   .attr("stroke-width", 2)
-  //   .attr("d", line()
-  //     .x(function (d) { return x(d.x) })
-  //     .y(function (d) { return y(d.y) }));
-
-  // // Add the points
-  // line_svg
+  // pct1_svg
   //   .append("g")
   //   .selectAll("dot")
-  //   .data(data)
+  //   .data(d_summary)
   //   .enter()
   //   .append("circle")
-  //   .attr("cx", function (d) { return x(d.x) })
-  //   .attr("cy", function (d) { return y(d.y) })
-  //   .attr("r", 3)
+  //   .attr("cx", x(d_summary[0]['ENROLLMENT_PCT']))
+  //   .attr("cy", y(5))
+  //   .attr("r", 5)
   //   .attr("fill", "steelBlue");
 
-  // text = line_svg.selectAll("text")
-  //   .append(g)
-  //   .data(data)
-  //   .enter()
-  //   .append("text")
-  //   .text(function (d) { return x(d.y) })
-  //   // .text('lab')
-  //   .attr('x', function (d) { return x(d.x) + 10 })
-  //   .attr('y', function (d) { return y(d.y) + 10 });
+  // pct1_svg
+  //   .append('text')
+  //   .attr('class', 'footnote')
+  //   .attr("transform", `translate(0,${height + 2 * (margin.bottom)})`)
+  //   .text('Percentile compared to all schools; hover for more details');
+
+  // LINE GRAPH
+  // MOSTLY WORKS: QUESTIONS: TEXT LABELS NOT APPEARING
+  const margin = { top: 20, right: 30, bottom: 10, left: 60 },
+    width = 400 - margin.left - margin.right,
+    height = 150 - margin.top - margin.bottom;
+
+  const line_svg = select("#app")
+    .append('svg')
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", width + margin.top + margin.bottom)
+    .append('g')
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+  line_svg.append('text')
+    .attr('class', 'chart-title')
+    .text('Total Incidents by School Year');
+
+  const x_array = [2014, 2015, 2016, 2017]
+  const y_array = [d_annual[0]['2014_TOTAL_INC'], d_annual[0]['2015_TOTAL_INC'], d_annual[0]['2016_TOTAL_INC'], d_annual[0]['2017_TOTAL_INC']]
+
+  const ydim = extent(y_array);
+
+  let data = []
+
+  for (let i = 0; i < x_array.length; i++) {
+    let ob = { x: x_array[i], y: y_array[i] }
+    data.push(ob)
+  }
+
+  const x = scaleLinear().domain([2013.5, 2017.5]).range([0, width])
+  line_svg.append('g')
+    .attr("transform", "translate(0," + height + ")")
+    .call(axisBottom(x)
+      .ticks()
+      .tickFormat(format('d'))
+      .tickValues([2014, 2015, 2016, 2017]));
+
+  const y = scaleLinear().domain([ydim[0] - 100, +ydim[1] + 100]).range([height, 0])
+  // line_svg.append('g') //keep commented out
+  //   .call(axisLeft(y)) //keep commented out
+  //   .attr('transform', `translate(50,10)`); //keep commented out
+
+  line_svg.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", 'steelBlue')
+    .attr("stroke-width", 2)
+    .attr("d", line()
+      .x(function (d) { return x(d.x) })
+      .y(function (d) { return y(d.y) }));
+
+  // Add the points
+  line_svg
+    .append("g")
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) { return x(d.x) })
+    .attr("cy", function (d) { return y(d.y) })
+    .attr("r", 3)
+    .attr("fill", "steelBlue");
+
+  line_svg.selectAll("text")
+    .append('g')
+    .data(data)
+    .enter()
+    .append("text")
+    .text(function (d) { return x(d.y) })
+    // .text('lab')
+    .attr('x', function (d) { return x(d.x) + 10 })
+    .attr('y', function (d) { return y(d.y) + 10 });
 
 
   // // PROPORTION PLOT – WORKS; HOW TO ORGANIZE SVGS??
