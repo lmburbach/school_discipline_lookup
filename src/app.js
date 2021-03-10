3e50dd8485634a0034f64ab6afc394f4eb330a5c
LauraMargaret Burbach <burbach@uchicago.edu>
2021-03-10 10:22:02 -0600

    Pushing hard code for reference

--- src/app.js
+++ src/app.js
@@ -18,98 +18,174 @@ function myVis(results) {
   const height = (36 / 24) * width;
   const margin = { top: 10, left: 50, right: 10, bottom: 50 };
 
-  // create data
-
   // create svg element:
+  const plotHeight = 500;
   const svg = select("#app")
     .append("svg")
     .attr("width", 500)
-    .attr("height", 500);
+    .attr("height", plotHeight);
 
-  // hard coding trapezoids for office hours
   const x0 = 100
   const x1 = 400
 
+  // First Element
   svg.append('polygon')
     .attr('points', `${x0},0
-    ${x1},0
-    ${x1},${d_subgroup[0]['%_DISC_POP']}
-    ${x0},${d_subgroup[0]['%_OVERALL_POP']}`);
+        ${x1},0
+        ${x1},${(d_subgroup[0]['%_DISC_POP'] / 100) * plotHeight}
+        ${x0},${(d_subgroup[0]['%_OVERALL_POP'] / 100) * plotHeight}`)
+    .attr('class', 'first');
 
   svg.append('text')
     .attr('x', 50)
-    .attr('y', `${(d_subgroup[0]['%_OVERALL_POP']) / 2}`)
+    .attr('y', `${(d_subgroup[0]['CUM_%_OVERALL'] / 100) * plotHeight}`)
     .text(`${(d_subgroup[0]['%_OVERALL_POP'])}%`);
 
   svg.append('text')
     .attr('x', 405)
-    .attr('y', `${(d_subgroup[0]['%_DISC_POP']) / 2}`)
-    .text(`${(d_subgroup[0]['%_OVERALL_POP'])}%`);
+    .attr('y', `${(d_subgroup[0]['CUM_%_DISC'] / 100) * plotHeight}`)
+    .text(`${(d_subgroup[0]['%_DISC_POP'])}%`);
 
+  // Second element – ABSTRACT FROM HERE
   svg.append('polygon')
-    .attr('points', `${x0},${d_subgroup[0]['%_OVERALL_POP']}
-    ${x1}, ${d_subgroup[0]['%_DISC_POP']}
-    ${x1}, ${d_subgroup[1]['CUM_%_DISC']}
-    ${x0}, ${d_subgroup[1]['CUM_%_OVERALL']}`)
-    .attr('fill', 'purple');
+    .attr('points', `${x0},${(d_subgroup[0]['CUM_%_OVERALL'] / 100) * plotHeight}
+    ${x1}, ${(d_subgroup[0]['CUM_%_DISC'] / 100) * plotHeight}
+    ${x1}, ${(d_subgroup[1]['CUM_%_DISC'] / 100) * plotHeight}
+    ${x0}, ${(d_subgroup[1]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+    .attr('class', 'second');
+
+  svg.append('text')
+    .attr('x', 50)
+    .attr('y', `${(d_subgroup[1]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+    .text(`${(d_subgroup[1]['%_OVERALL_POP'])}%`);
+
+  svg.append('text')
+    .attr('x', 405)
+    .attr('y', `${(d_subgroup[1]['CUM_%_DISC'] / 100) * plotHeight}`)
+    .text(`${(d_subgroup[1]['%_DISC_POP'])}%`);
+
+  // FOR SUBGROUP CATEGORY = RACE; add others
+  const ordinalClasses = ['third', 'fourth', 'fifth', 'sixth', 'seventh']
+
+  if (d_subgroup.length > 2) {
+    for (let i = 0; i < ordinalClasses.length; i++) {
+      if (d_subgroup[i + 2]['%_OVERALL_POP'] > 0) {
+        svg.append('polygon')
+          .attr('points', `${x0},${(d_subgroup[i + 1]['CUM_%_OVERALL'] / 100) * plotHeight}
+        ${x1}, ${(d_subgroup[i + 1]['CUM_%_DISC'] / 100) * plotHeight}
+        ${x1}, ${(d_subgroup[i + 2]['CUM_%_DISC'] / 100) * plotHeight}
+        ${x0}, ${(d_subgroup[i + 2]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+          .attr('class', ordinalClasses[i]);
+
+        svg.append('text')
+          .attr('x', 50)
+          .attr('y', `${(d_subgroup[i + 2]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+          .text(`${(d_subgroup[i + 2]['%_OVERALL_POP'])}%`);
+
+        svg.append('text')
+          .attr('x', 405)
+          .attr('y', `${(d_subgroup[i + 2]['CUM_%_DISC'] / 100) * plotHeight}`)
+          .text(`${(d_subgroup[i + 2]['%_DISC_POP'])}%`);
+      }
+    }
+  }
+
+
+  // ///WORKS BELOW
+  // if (d_subgroup.length > 2) {
+  //   if (d_subgroup[2]['%_OVERALL_POP'] > 0) {
+  //     svg.append('polygon')
+  //       .attr('points', `${x0},${(d_subgroup[1]['CUM_%_OVERALL'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[1]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[2]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x0}, ${(d_subgroup[2]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .attr('class', 'third');
+
+  //     svg.append('text')
+  //       .attr('x', 50)
+  //       .attr('y', `${(d_subgroup[2]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[2]['%_OVERALL_POP'])}%`);
+
+  //     svg.append('text')
+  //       .attr('x', 405)
+  //       .attr('y', `${(d_subgroup[2]['CUM_%_DISC'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[2]['%_DISC_POP'])}%`);
+  //   }
+
+  //   if (d_subgroup[3]['%_OVERALL_POP'] > 0) {
+  //     svg.append('polygon')
+  //       .attr('points', `${x0},${(d_subgroup[2]['CUM_%_OVERALL'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[2]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[3]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x0}, ${(d_subgroup[3]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .attr('class', 'fourth');
+
+  //     svg.append('text')
+  //       .attr('x', 50)
+  //       .attr('y', `${(d_subgroup[3]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[3]['%_OVERALL_POP'])}%`);
+
+  //     svg.append('text')
+  //       .attr('x', 405)
+  //       .attr('y', `${(d_subgroup[3]['CUM_%_DISC'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[3]['%_DISC_POP'])}%`);
+  //   }
+
+  //   if (d_subgroup[4]['%_OVERALL_POP'] > 0) {
+  //     svg.append('polygon')
+  //       .attr('points', `${x0},${(d_subgroup[3]['CUM_%_OVERALL'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[3]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[4]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x0}, ${(d_subgroup[4]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .attr('class', 'fifth');
+
+  //     svg.append('text')
+  //       .attr('x', 50)
+  //       .attr('y', `${(d_subgroup[4]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[4]['%_OVERALL_POP'])}%`);
+
+  //     svg.append('text')
+  //       .attr('x', 405)
+  //       .attr('y', `${(d_subgroup[4]['CUM_%_DISC'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[4]['%_DISC_POP'])}%`);
+  //   }
+  //   //idx 5 sixth
+  //   if (d_subgroup[5]['%_OVERALL_POP'] > 0) {
+  //     svg.append('polygon')
+  //       .attr('points', `${x0},${(d_subgroup[4]['CUM_%_OVERALL'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[4]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[5]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x0}, ${(d_subgroup[5]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .attr('class', 'sixth');
+
+  //     svg.append('text')
+  //       .attr('x', 50)
+  //       .attr('y', `${(d_subgroup[5]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[5]['%_OVERALL_POP'])}%`);
+
+  //     svg.append('text')
+  //       .attr('x', 405)
+  //       .attr('y', `${(d_subgroup[5]['CUM_%_DISC'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[5]['%_DISC_POP'])}%`);
+  //   }
+  //   ///idx 6 = 7
+  //   if (d_subgroup[6]['%_OVERALL_POP'] > 0) {
+  //     svg.append('polygon')
+  //       .attr('points', `${x0},${(d_subgroup[5]['CUM_%_OVERALL'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[5]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x1}, ${(d_subgroup[6]['CUM_%_DISC'] / 100) * plotHeight}
+  //       ${x0}, ${(d_subgroup[6]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .attr('class', 'seventh');
+
+  //     svg.append('text')
+  //       .attr('x', 50)
+  //       .attr('y', `${(d_subgroup[6]['CUM_%_OVERALL'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[6]['%_OVERALL_POP'])}%`);
+
+  //     svg.append('text')
+  //       .attr('x', 405)
+  //       .attr('y', `${(d_subgroup[6]['CUM_%_DISC'] / 100) * plotHeight}`)
+  //       .text(`${(d_subgroup[6]['%_DISC_POP'])}%`);
+  //   }
 
 }
-
-
-//   svg.append('g')
-//     .selectAll('g')
-//     .data(d_subgroup.columns)
-//     .join('g')
-//     .attr('transform', (d, i) => `translate(${ d(i) }, 20)`)
-//     .call(g => g.append('text').text(d => d))
-//     .call(g => g.append('line').attr('y1', 3).attr('y2', 9));
-// }
-
-  // for (let i of Array(d_subgroup.length - 1).keys()) {
-  //   const x0 = 100;
-  //   const x1 = 400;
-
-  //   if (i === 0) {
-  //     var ay = 0;
-  //     var by = 0;
-  //     var cy = d_subgroup[0]['%_DISC_POP'];
-  //     var dy = d_subgroup[0]['%_OVERALL_POP'];
-
-  //     svg.append('polygon')
-  //       .attr('points', `${ x0 }, ${ ay } ${ x1 }, ${ by } ${ x1 }, ${ cy } ${ x0 }, ${ dy }`);
-
-  //   }
-
-  //   else {
-  //     var ay = d_subgroup[i - 1]['%_DISC_POP'];
-  //     var dy = d_subgroup[i - 1]['%_OVERALL_POP'];
-  //     var cy = d_subgroup[i]['%_DISC_POP'];
-  //     var dy = d_subgroup[i]['%_OVERALL_POP'];
-
-  //     svg.append('polygon')
-  //       .attr('points', `${ x0 }, ${ ay } ${ x1 }, ${ by } ${ x1 }, ${ cy } ${ x0 }, ${ dy }`);
-  //   }
-
-
-
-  // }
-
-
-
-  // const svg = select('#app')
-  //   .append(svg)
-  //   .attr('height', `${ height }px`)
-  //   .attr('width', `${ width }px`)
-  //   .append('g')
-  //   .attr("transform",
-  //     "translate(" + margin.left + "," + margin.top + ")");
-
-  // svg.append('path')
-  //   .attr('d', area()
-  //     .x0(0)
-  //     .x1(0)
-  //     .y1(61.4)
-  //     .y2(41.8))
-
-  // const test = d_subgroup[0]
-  // console.log(test)
